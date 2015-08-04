9bf56c802864e61f42c1fbf2c1cbf1a7f0a31323
Adam Fluke <fluke.adam@gmail.com>
2015-08-04 10:16:13 -0700

    Create user model/migration/controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150802180905) do
+ActiveRecord::Schema.define(version: 20150804142843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20150802180905) do
     t.integer  "answer_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.integer  "pin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
